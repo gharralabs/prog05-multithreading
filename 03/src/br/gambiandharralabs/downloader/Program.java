cd25182a96db4425f79723935a03e6611a9220a1
Marine King <mkp@prime.co.kr>
2015-08-05 12:50:40 -0300

    Downloader - final

--- 03/src/br/gambiandharralabs/downloader/Program.java
+++ 03/src/br/gambiandharralabs/downloader/Program.java
@@ -1,15 +1,10 @@
 package br.gambiandharralabs.downloader;  
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Scanner;
 
 public class Program
 {
-    public static void main(String[] args) throws IOException 
+    public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
         
@@ -18,31 +13,8 @@ public class Program
             System.out.println("Informe a URL");
             String endereco = scanner.nextLine();
             
-            try
-            {
-                URL url = new URL(endereco);
-                InputStream is = url.openStream();
-                
-                String nomeArquivo;
-                nomeArquivo = endereco.substring( endereco.lastIndexOf("/") + 1, endereco.length());
-                
-		FileOutputStream fos = new FileOutputStream("./" + nomeArquivo);
-		byte[] buffer = new byte[1024];
-		int len = is.read(buffer);
-		while (len != -1) {
-		    fos.write(buffer, 0, len);
-		    len = is.read(buffer);
-		}
-		is.close();
-		fos.close();
-                
-                System.out.println("Arquivo: " + nomeArquivo + " foi transferido"
-                                    + " com sucesso.");
-            }
-            catch(MalformedURLException ex)
-            {
-                System.out.println("Endereço invalido");
-            }
+            TransferenciaRunnable tr = new TransferenciaRunnable(endereco);
+            new Thread(tr).start();
         }
     }
 }
---
+++ 03/src/br/gambiandharralabs/downloader/TransferenciaRunnable.java
@@ -0,0 +1,59 @@
+package br.gambiandharralabs.downloader;  
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class TransferenciaRunnable implements Runnable 
+{
+    private final String recurso;
+    
+    public TransferenciaRunnable( String recurso )
+    {
+        this.recurso = recurso;
+    }
+    
+    @Override
+    public void run()
+    {
+        try
+        {
+            URL url = new URL(recurso);
+            InputStream is = url.openStream();
+
+            String nomeArquivo;
+            nomeArquivo = recurso.substring(recurso.lastIndexOf("/") + 1,
+                                            recurso.length());
+
+            FileOutputStream fos = new FileOutputStream("./" + nomeArquivo);
+            byte[] buffer = new byte[1024];
+            int len = is.read(buffer);
+            while (len != -1)
+            {
+                fos.write(buffer, 0, len);
+                len = is.read(buffer);
+            }
+            is.close();
+            fos.close();
+
+            System.out.println("Arquivo: " + nomeArquivo + " foi transferido"
+                    + " com sucesso.");
+        }
+        catch (MalformedURLException ex)
+        {
+            Logger.getLogger("Endereço invalido" + 
+                             TransferenciaRunnable.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Endereço invalido");
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(TransferenciaRunnable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
